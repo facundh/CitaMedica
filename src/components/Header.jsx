a44e2a6f372aee51976f0e2d94966acbe3e2a49e
Facundo Herrera <facundhfed@gmail.com>
2022-04-01 22:32:51 +0200

    Tailwind Instalado

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Listado Pacientes <span>Consultorio</span></h1>
+
+    </div>
+  )
+}
+
+export default Header
